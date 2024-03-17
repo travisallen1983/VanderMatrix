ea12be7548a14bc474ba6276f6285265c6d4eed0
travisallen1983 <51452584+travisallen1983@users.noreply.github.com>
2024-03-16 21:46:53 -0500

    Create vander_matrix.py

---
+++ vander_matrix.py
@@ -0,0 +1,23 @@
+def vander_matrix_lists(input_list: list[int], input_range: int) -> list[list]:
+    output_list = [[list_item ** x for x in range(input_range)][::-1] for list_item in input_list]
+    return output_list
+
+
+vander_output = vander_matrix_lists([1, 2, 3, 4], 7)
+for row in vander_output:
+    print(row)
+
+
+def vander_matrix_loops(input_list: list[int], input_range: int) -> list[list]:
+    output_list = []
+    for list_item in input_list:
+        _list = []
+        for i in range(input_range - 1, -1, -1):
+            _list.append(list_item ** i)
+        output_list.append(_list)
+    return output_list
+
+
+vander_output = vander_matrix_loops([1, 2, 3, 4], 7)
+for row in vander_output:
+    print(row)
